0bfd20229dc3e1ca955072f4d22ebab1495060a1
Michael Chinn <mchinn614@gmail.com>
2019-03-08 14:06:45 -0800

    updated db source to cluster

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 "use strict";
 const DATABASE_URL =
-  process.env.DATABASE_URL || "mongodb://localhost:27017/blog";
+  process.env.DATABASE_URL ||
+  "mongodb+srv://user1:user1@cluster0-yhars.mongodb.net/blog?retryWrites=true";
 module.exports = DATABASE_URL;
