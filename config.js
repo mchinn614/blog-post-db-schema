38018d6c23f5a378fb209a9ac7b98cd2df27f105
Michael Chinn <mchinn614@gmail.com>
2019-03-10 17:57:20 -0700

    added new db reference

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 "use strict";
 const DATABASE_URL =
   process.env.DATABASE_URL ||
-  "mongodb+srv://user1:user1@cluster0-yhars.mongodb.net/blog?retryWrites=true";
+  "mongodb+srv://user1:user1@cluster0-yhars.mongodb.net/blog-1?retryWrites=true";
 module.exports = DATABASE_URL;
